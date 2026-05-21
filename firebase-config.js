27130f12060a793d272ffea99b90e44938d6d630
vadimm461 <vadimm461@gmail.com>
2026-05-21 10:06:47 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+
+apiKey: "AIzaSyBZ-AW6XoMR14KmBtlz2q06Z0jPGXnWMTw",
+
+authDomain: "auto-style-4dbb7.firebaseapp.com",
+
+projectId: "auto-style-4dbb7",
+
+storageBucket: "auto-style-4dbb7.firebasestorage.app",
+
+messagingSenderId: "217023127803",
+
+appId: "1:217023127803:web:78568cf161edd452b0905e"
+
+};
