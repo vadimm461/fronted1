f3422fe87ec19cad76584f5ae75485bc0d061212
vadimm461 <vadimm461@gmail.com>
2026-05-24 21:20:54 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBZ-AW6XoMR14KmBtlz2q06Z0jPGXnWMTw",
   authDomain: "auto-style-4dbb7.firebaseapp.com",
   databaseURL: "https://auto-style-4dbb7-default-rtdb.europe-west1.firebasedatabase.app",
